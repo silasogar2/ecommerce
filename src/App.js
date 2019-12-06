c190cab07745d3a2249cc8ca795133ceeb4987b0
jendyjasper <silasogar2@gmail.com>
2019-12-06 16:48:18 +0100

    Latest

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 // import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <HomePage/>
     </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,51 +2,42 @@ import React from 'react';
 
 import './homepage.style.scss';
 const HomePage = () => (
-    <div className="homepage">
-        <div className="directory-manu">
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">Hat</h1>
-                    <span className="subtitle"> Shop Now</span>
-                </div>
-            </div>
-
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">Hat</h1>
-                    <span className="subtitle"> Shop Now</span>
-                </div>
-            </div>
-
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">Hat</h1>
-                    <span className="subtitle"> Shop Now</span>
-                </div>
-            </div>
-
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">Jackets</h1>
-                    <span className="subtitle"> Shop Now</span>
-                </div>
-            </div>
-
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">Shoes</h1>
-                    <span className="subtitle"> Shop Now</span>
-                </div>
-            </div>
-
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">Toiletries</h1>
-                    <span className="subtitle"> Shop Now</span>
-                </div>
-            </div>
+    <div className='homepage'>
+    <h1>Welcome to my Homepage</h1>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>HATS</div>
+          <span className='subtitle'>SHOP NOW</span>
         </div>
+      </div>
+      
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>JACKETS</div>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>SHOES</div>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>WOMENS</div>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <div className='title'>MENS</div>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
     </div>
-)
+  </div>
+);
 
 export default HomePage;
