26539443da6f5428b59f51f564ac10bb01a7f264
jendyjasper <silasogar2@gmail.com>
2019-12-06 16:16:13 +0100

    React App Ecommerce

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import './homepage.style.scss';
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-manu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Hat</h1>
+                    <span className="subtitle"> Shop Now</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Hat</h1>
+                    <span className="subtitle"> Shop Now</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Hat</h1>
+                    <span className="subtitle"> Shop Now</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Jackets</h1>
+                    <span className="subtitle"> Shop Now</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Shoes</h1>
+                    <span className="subtitle"> Shop Now</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Toiletries</h1>
+                    <span className="subtitle"> Shop Now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
